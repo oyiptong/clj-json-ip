0af1ceffe7cb143ce1b841ba880d955a704fb833
Olivier Yiptong <olivier@olivieryiptong.com>
2013-07-04 15:24:25 -0400

    obtaining headers set from load balancers

--- src/json_ip/main.clj
+++ src/json_ip/main.clj
@@ -3,8 +3,14 @@
         ring.middleware.json
         ring.adapter.jetty))
 
-(defn handler [req]
-  (response {:ip (:remote-addr req)}))
+(defn handler [request]
+  (if-let [xri (get-in request [:headers "x-real-ip"])]
+    (response {:ip xri})
+    (if-let [xra (get-in request [:headers "x-remote-addr"])]
+      (response {:ip xra})
+      (if-let [xff (get-in request [:headers "x-forwarded-for"])]
+        (response {:ip xff})
+        (response {:ip (:remote-addr request)})))))
 
 (def app
   (wrap-json-response handler))
